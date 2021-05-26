e57b553fc997cbd617b687f8cdd3744f2577c752
CGalko <cash.galko12@ncf.edu>
2021-05-25 21:55:26 -0400

    got the code for header and main to function. Just need to get data in and then style it.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../styles/Header.css";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div id="headerDiv">
+                <h2 id="headerText">Employee Directory</h2>
+            </div>
+        );
+    }
+}
